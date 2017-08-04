569ac48e4738b8fc86be7c8055c45a7337c8204b
WMXPY <wm@wmpcxpy.com>
2017-08-04 14:28:01 -0500

    initial to npm package

---
+++ src/import.js
@@ -0,0 +1,13 @@
+import {
+    TitleComponent
+} from './out/title/title.component';
+import {
+    BlockComponent
+} from './out/block/block.component';
+import {
+    ButtonComponent
+} from './out/button/button.component';
+
+export const title = TitleComponent;
+export const block = BlockComponent;
+export const button = ButtonComponent;
